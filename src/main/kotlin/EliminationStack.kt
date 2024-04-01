b9fe31e90726a4a6f639934cf9a805d1bfffe4e0
Lesh79 <Temerlan.axmetov.75@mail.ru>
2024-04-01 21:53:35 +0300

    Add: fix warnings

--- src/main/kotlin/EliminationStack.kt
+++ src/main/kotlin/EliminationStack.kt
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicReference
 enum class Operations {PUSH, POP}
 
 class EliminationStack<T> : Stack<T> {
-    private val eliminationArray = arrayOfNulls<AtomicReference<ThreadInfo<T>>>(32)
+    private val eliminationArray = arrayOfNulls<AtomicReference<ThreadInfo<T>>>(1000)
     private val top = AtomicReference<Node<T>>(null)
     private val collision = arrayOfNulls<AtomicReference<Int>>(6)
 
@@ -21,7 +21,7 @@ class EliminationStack<T> : Stack<T> {
     override fun peek(): T?{
         return top.get().value
     }
-    fun TryCollision(p: ThreadInfo<T>, q: ThreadInfo<T>): Boolean {
+    private fun TryCollision(p: ThreadInfo<T>, q: ThreadInfo<T>): Boolean {
         if (p.op == Operations.PUSH){
             val him = p.mypid
             return eliminationArray[him.toInt()]?.compareAndSet(q, p) == true
@@ -36,33 +36,33 @@ class EliminationStack<T> : Stack<T> {
         }
          return false
     }
-    fun FinishCollision(p: ThreadInfo<T>){
+    private fun FinishCollision(p: ThreadInfo<T>){
         if (p.op == Operations.POP){
             p.node = eliminationArray[p.mypid.toInt()]?.get()?.node
             eliminationArray[p.mypid.toInt()] = null
         }
     }
 
-    suspend fun StackOp(p: ThreadInfo<T>){
-        if (TryPerformStackOp(p) == false)
+    private suspend fun StackOp(p: ThreadInfo<T>){
+        if (!TryPerformStackOp(p))
             LesOp(p)
         return
     }
-    suspend fun LesOp (p: ThreadInfo<T>){
+    private suspend fun LesOp (p: ThreadInfo<T>){
         while (true){
             eliminationArray[p.mypid.toInt()] = AtomicReference(p)
-            var pos = collision.random()?.get()!!
+            val pos = collision.random()?.get()!!
             var him = collision[pos]
             while (collision[pos]?.compareAndSet(him?.get(), p.mypid.toInt()) == false)
                 him = collision[pos]
             if (him != null){
-                var q = eliminationArray[him.get()]?.get()
+                val q = eliminationArray[him.get()]?.get()
                 if (q != null && q.mypid.toInt() == him.get() && q.op != p.op){
                     if (eliminationArray[p.mypid.toInt()]?.compareAndSet(p, null) == true){
-                        if (TryCollision(p,q) == true)
+                        if (TryCollision(p,q))
                             return
                         else {
-                            if (TryPerformStackOp(p) == true){
+                            if (TryPerformStackOp(p)){
                                 return
                             }
                             continue
@@ -79,29 +79,29 @@ class EliminationStack<T> : Stack<T> {
                 FinishCollision(p)
                 return
             }
-            if (TryPerformStackOp(p) == true)
+            if (TryPerformStackOp(p))
                 return
         }
     }
 
-    fun TryPerformStackOp(p: ThreadInfo<T>): Boolean {
-        val phead : Node<T>?
-        val pnext : Node<T>?
+    private fun TryPerformStackOp(p: ThreadInfo<T>): Boolean {
+        val pHead : Node<T>?
+        val pNext : Node<T>?
 
         if (p.op == Operations.PUSH){
-            phead = this.top.get()
-            p.node?.next = phead
-            return this.top.compareAndSet(phead, p.node)
+            pHead = this.top.get()
+            p.node?.next = pHead
+            return this.top.compareAndSet(pHead, p.node)
         }
         if(p.op == Operations.POP){
-            phead = this.top.get()
-            if(phead == null){
+            pHead = this.top.get()
+            if(pHead == null){
                 p.node = null
                 return true
             }
-            pnext = phead.next
-            if (this.top.compareAndSet(phead, pnext)) {
-                p.node = phead
+            pNext = pHead.next
+            if (this.top.compareAndSet(pHead, pNext)) {
+                p.node = pHead
                 return true
             }
         }
